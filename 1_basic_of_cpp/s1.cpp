509b8a85977cc2be50b41d596add8d9e2fde6918
AIxi0425 <aixi0425@gmail.com>
2016-07-16 15:29:39 +0800

    Add 1_basic_of_cpp to the repository

---
+++ 1_basic_of_cpp/s1.cpp
@@ -0,0 +1,24 @@
+/*
+ * Structure of a program
+ * this is my first program in C++ 
+ */
+
+#define SEG2
+
+#ifdef SEG1
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello,world";
+}
+
+#elif defined SEG2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "I am a C++ program";
+}
+#endif
