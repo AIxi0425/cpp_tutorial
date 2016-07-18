f04a911491022135a6ec05e0b59d75cda1f28c3c
AIxi0425 <aixi0425@gmail.com>
2016-07-18 11:11:56 +0800

    change s2 and s3

--- 1_basic_of_cpp/s2.cpp
+++ 1_basic_of_cpp/s2.cpp
@@ -12,18 +12,18 @@ using namespace std;
 
 int main()
 {
-	int a, b;
-	int result;
-	
-	a = 5;
-	b = 2;
-	a = a + 1;
-	result = a - b;
-
-	cout << result;
-	
-	/* terminate the program */
-	return 0;
+    int a, b;
+    int result;
+    
+    a = 5;
+    b = 2;
+    a = a + 1;
+    result = a - b;
+    
+    cout << result;
+    
+    /* terminate the program */
+    return 0;
 }
 
 /***************************/
@@ -34,22 +34,22 @@ using namespace std;
 
 int main()
 {
-	int a = 5;     // initial value:5
-	int b(3);      // initial value:3
-	int c{2};      // initial value:2
-	int result;    // initial value undetermined
-
-	a = a + b;
-	result = a - c;
-	cout << result;
-	
-	/* type deduction: auto and decltype */
-	int foo = 0;
-	auto bar = foo;    // the same as: int bar = foo;
-	int foo = 0;
-	decltype(foo) bar; // the sme as: int bar;
-
-	return 0;
+    int a = 5;     // initial value:5
+    int b(3);      // initial value:3
+    int c{2};      // initial value:2
+    int result;    // initial value undetermined
+    
+    a = a + b;
+    result = a - c;
+    cout << result;
+    
+    /* type deduction: auto and decltype */
+    int foo = 0;
+    auto bar = foo;    // the same as: int bar = foo;
+    int foo = 0;
+    decltype(foo) bar; // the sme as: int bar;
+    
+    return 0;
 }
 
 /***************************/
@@ -61,12 +61,12 @@ using namespace std;
 
 int main()
 {
-	string mystring;
-	mystring = "This is the initial string content";
-	cout << mystring << endl;
-	mystring = "This is a different string content";
-	cout << mystring << endl;
-	
-	return 0;
+    string mystring;
+    mystring = "This is the initial string content";
+    cout << mystring << endl;
+    mystring = "This is a different string content";
+    cout << mystring << endl;
+    
+    return 0;
 }
 #endif
---
+++ 1_basic_of_cpp/s3.cpp
@@ -0,0 +1,47 @@
+/*
+ * Constants
+ */
+
+#define SEG1
+
+/***************************/
+
+#ifdef SEG1
+#include <iostream>
+using namespace std;
+
+const double pi = 3.14159;
+const char newline = '\n';
+
+int main()
+{
+	double r = 5.0;
+	double circle;
+
+	circle = 2 * pi * r;
+	cout << circle;
+	cout << newline;
+
+}
+
+/***************************/
+
+#elif defined SEG2
+#include <iostream>
+using namespace std;
+
+#define PI 3.14159
+#define NEWLINE '\n'
+
+int main()
+{
+	double r = 5.0;
+	double circle;
+	
+	circle = 2 * PI * r;
+	cout << circle;
+	cout << NEWLINE;
+
+}
+
+#endif
