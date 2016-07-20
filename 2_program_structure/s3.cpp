9ee8bfa32c9f6f7f538d38ffff5439adbebaee01
AIxi0425 <aixi0425@gmail.com>
2016-07-20 21:56:56 +0800

    change s3.cpp

--- 2_program_structure/s3.cpp
+++ 2_program_structure/s3.cpp
@@ -2,7 +2,7 @@
  * Overloads and tmplates
  */
 
-#define SEG2
+#define SEG3
 
 /***************************/
 
@@ -50,6 +50,32 @@ int main()
 {
 	cout << sum(10, 20) << endl;
 	cout << sum(1.5, 1.0) << endl;
+
+    return 0;
+}
+
+/***************************/
+
+#elif defined SEG3
+#include <iostream>
+using namespace std;
+
+template <class T>
+T sum (T a, T b)
+{
+    T result;
+	result = a + b;
+	return result;
+}
+
+int main()
+{
+    int i = 5, j = 6, k;
+	double f = 2.0, g = 0.5, h;
+	k = sum<int>(i, j);
+	h = sum<double>(f, g);
+	cout << k << endl;
+	cout << h << endl;
 
     return 0;
 }
