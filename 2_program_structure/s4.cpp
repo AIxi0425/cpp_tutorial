b1e4e24b543a9d70749a2cfd31c20878bd779e61
AIxi0425 <aixi0425@gmail.com>
2016-07-21 13:30:29 +0800

    add s4.cpp

--- 2_program_structure/s4.cpp
+++ 2_program_structure/s4.cpp
@@ -13,18 +13,18 @@ using namespace std;
 int main()
 {
     int x = 10;
-	int y = 20;
+    int y = 20;
 	{
 	    int x;
-		x = 50;
-		y = 50;
-		cout << "inner block" << endl;
-		cout << "x: " << x << endl;
-		cout << "y: " << y << endl;
+	    x = 50;
+	    y = 50;
+	    cout << "inner block" << endl;
+	    cout << "x: " << x << endl;
+	    cout << "y: " << y << endl;
 	}
 
-	cout << "outer block" << endl;
-	cout << "x: " << x << endl;
+    cout << "outer block" << endl;
+    cout << "x: " << x << endl;
     cout << "y: " << y << endl;
 
     return 0;
@@ -39,24 +39,24 @@ using namespace std;
 namespace first
 {
     int x = 5;
-	int y = 10;
+    int y = 10;
 }
 
 namespace second
 {
     double x = 3.14;
-	double y = 2.71;
+    double y = 2.71;
 }
 
 int main()
 {
     using namespace first;
-	cout << x << endl;
-	cout << y << endl;
-	cout << second::x << endl;
-	cout << second::y << endl;
+    cout << x << endl;
+    cout << y << endl;
+    cout << second::x << endl;
+    cout << second::y << endl;
 
-	return 0;
+    return 0;
 }
 
 /***************************/
@@ -70,9 +70,10 @@ int x;
 int main()
 {
     int y;
-	cout << x << endl;
-	cout << y << endl;
-
-	return 0;
+    cout << x << endl;
+    cout << y << endl;
+    
+    return 0;
 }
+
 #endif
