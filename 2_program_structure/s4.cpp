bbddba9cd070c69b5a09e103091e3620615fca0c
AIxi0425 <aixi0425@gmail.com>
2016-07-21 13:32:25 +0800

    add s4.cpp

--- 2_program_structure/s4.cpp
+++ 2_program_structure/s4.cpp
@@ -14,14 +14,14 @@ int main()
 {
     int x = 10;
     int y = 20;
-	{
-	    int x;
-	    x = 50;
-	    y = 50;
-	    cout << "inner block" << endl;
-	    cout << "x: " << x << endl;
-	    cout << "y: " << y << endl;
-	}
+    {
+        int x;
+        x = 50;
+        y = 50;
+        cout << "inner block" << endl;
+        cout << "x: " << x << endl;
+        cout << "y: " << y << endl;
+    }
 
     cout << "outer block" << endl;
     cout << "x: " << x << endl;
