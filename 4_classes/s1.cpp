e55c6f642b1ec9916efbc91c08eb0fde14fa0892
AIxi0425 <aixi0425@gmail.com>
2016-07-24 17:47:21 +0800

    classes(1)

--- 4_classes/s1.cpp
+++ 4_classes/s1.cpp
@@ -2,7 +2,7 @@
  * Classes
  */
 
-#define SEG1
+#define SEG2
 
 /***************************/
 
@@ -12,7 +12,7 @@ using namespace std;
 
 class Rectangle {
 	int width, heigth;
-  public:
+public:
   	void set_values (int, int);
 	int area()
 	{
@@ -30,6 +30,37 @@ int main()
 	Rectangle rect_a, rect_b;
 	rect_a.set_values (3, 4);
 	rect_b.set_values (5, 6);
+	cout << "rect_a area is " << rect_a.area() << endl;
+	cout << "rect_b area is " << rect_b.area() << endl;
+
+	return 0;
+}
+
+/***************************/
+
+#elif defined SEG2
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+	int width, heigth;
+public:
+	Rectangle (int, int);
+	int area ()
+	{
+		return width * heigth;
+	}
+};
+
+Rectangle::Rectangle (int x, int y)
+{
+	width = x;
+	heigth = y;
+}
+
+int main()
+{
+	Rectangle rect_a(3, 5), rect_b(10, 20);
 	cout << "rect_a area is " << rect_a.area() << endl;
 	cout << "rect_b area is " << rect_b.area() << endl;
 
