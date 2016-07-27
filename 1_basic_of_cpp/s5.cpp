476466924c5fada0a54351afe8769ed28aedb813
AIxi0425 <aixi0425@gmail.com>
2016-07-27 20:15:29 +0800

    Basic Input/Output

--- 1_basic_of_cpp/s5.cpp
+++ 1_basic_of_cpp/s5.cpp
@@ -5,8 +5,8 @@
 #define SEG3
 
 /***************************/
-
 #ifdef SEG1
+
 #include <iostream>
 using namespace std;
 
@@ -22,28 +22,28 @@ int main()
 }
 
 /***************************/
-
 #elif defined SEG2
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main()
 {
-    string mystr;
-    cout << "what's your name? ";
-    getline(cin,mystr);
-    cout << "Hello, " << mystr << ".\n";
-    cout << "what's your favorite team? ";
-    getline(cin,mystr);
-    cout << "I like " << mystr << " too!\n";
-
-    return 0;
+	string mystr;
+	cout << "what's your name? ";
+	getline(cin,mystr);
+	cout << "Hello, " << mystr << ".\n";
+	cout << "what's your favorite team? ";
+	getline(cin,mystr);
+	cout << "I like " << mystr << " too!\n";
+	
+	return 0;
 }
 
 /***************************/
-
 #elif defined SEG3
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -51,19 +51,21 @@ using namespace std;
 
 int main()
 {
-    string mystr;
-    float price = 0;
-    int quantity = 0;
-    
-    cout << "Enter the price: ";
-    getline(cin,mystr);
-    stringstream(mystr) >> price;
-    cout << "Enter the quantity: ";
-    getline(cin,mystr);
-    stringstream(mystr) >> quantity;
-    cout << "Total price: " << price*quantity << endl;
-    
-    return 0;	
+	string mystr;
+	float price = 0;
+	int quantity = 0;
+	
+	cout << "Enter the price: ";
+	getline(cin,mystr);
+	stringstream(mystr) >> price;
+	cout << "Enter the quantity: ";
+	getline(cin,mystr);
+	stringstream(mystr) >> quantity;
+	cout << "Total price: " << price*quantity << endl;
+	
+	return 0;	
 }
+
+/***************************/
 
 #endif
