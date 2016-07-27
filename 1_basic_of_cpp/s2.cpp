e4f2233c2cf9e5a1a86e330718eb70bf7915d3db
AIxi0425 <aixi0425@gmail.com>
2016-07-27 20:05:53 +0800

    Variables and types

--- 1_basic_of_cpp/s2.cpp
+++ 1_basic_of_cpp/s2.cpp
@@ -5,56 +5,56 @@
 #define SEG1
 
 /***************************/
-
 #ifdef SEG1
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int a, b;
-    int result;
-    
-    a = 5;
-    b = 2;
-    a = a + 1;
-    result = a - b;
-    
-    cout << result;
-    
-    /* terminate the program */
-    return 0;
+	int a, b;
+	int result;
+	
+	a = 5;
+	b = 2;
+	a = a + 1;
+	result = a - b;
+	
+	cout << result;
+	
+	/* terminate the program */
+	return 0;
 }
 
 /***************************/
-
 #elif defined SEG2
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int a = 5;     // initial value:5
-    int b(3);      // initial value:3
-    int c{2};      // initial value:2
-    int result;    // initial value undetermined
-    
-    a = a + b;
-    result = a - c;
-    cout << result;
-    
-    /* type deduction: auto and decltype */
-    int foo = 0;
-    auto bar = foo;    // the same as: int bar = foo;
-    int foo = 0;
-    decltype(foo) bar; // the sme as: int bar;
-    
-    return 0;
+	int a = 5;     // initial value:5
+	int b(3);      // initial value:3
+	int c{2};      // initial value:2
+	int result;    // initial value undetermined
+	
+	a = a + b;
+	result = a - c;
+	cout << result;
+	
+	/* type deduction: auto and decltype */
+	int foo = 0;
+	auto bar = foo;    // the same as: int bar = foo;
+	int foo = 0;
+	decltype(foo) bar; // the sme as: int bar;
+	
+	return 0;
 }
 
 /***************************/
-
 #elif defined SEG3
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -69,4 +69,7 @@ int main()
     
     return 0;
 }
+
+/***************************/
+
 #endif
