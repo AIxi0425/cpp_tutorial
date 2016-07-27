890e6a4ac25da898b38a0f6975fdd9e587dffd61
AIxi0425 <aixi0425@gmail.com>
2016-07-27 20:38:48 +0800

    Name Visibility

--- 2_program_structure/s4.cpp
+++ 2_program_structure/s4.cpp
@@ -5,75 +5,77 @@
  #define SEG3
 
 /***************************/
-
 #ifdef SEG1
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int x = 10;
-    int y = 20;
-    {
-        int x;
-        x = 50;
-        y = 50;
-        cout << "inner block" << endl;
-        cout << "x: " << x << endl;
-        cout << "y: " << y << endl;
-    }
-
-    cout << "outer block" << endl;
-    cout << "x: " << x << endl;
-    cout << "y: " << y << endl;
-
-    return 0;
+	int x = 10;
+	int y = 20;
+	{
+	    int x;
+	    x = 50;
+	    y = 50;
+	    cout << "inner block" << endl;
+	    cout << "x: " << x << endl;
+	    cout << "y: " << y << endl;
+	}
+	
+	cout << "outer block" << endl;
+	cout << "x: " << x << endl;
+	cout << "y: " << y << endl;
+	
+	return 0;
 }
 
 /***************************/
-
 #elif defined SEG2
+
 #include <iostream>
 using namespace std;
 
 namespace first
 {
-    int x = 5;
-    int y = 10;
+	int x = 5;
+	int y = 10;
 }
 
 namespace second
 {
-    double x = 3.14;
-    double y = 2.71;
+	double x = 3.14;
+	double y = 2.71;
 }
 
 int main()
 {
-    using namespace first;
-    cout << x << endl;
-    cout << y << endl;
-    cout << second::x << endl;
-    cout << second::y << endl;
-
-    return 0;
+	using namespace first;
+	cout << x << endl;
+	cout << y << endl;
+	cout << second::x << endl;
+	cout << second::y << endl;
+	
+	return 0;
 }
 
 /***************************/
-
 #elif defined SEG3
 #include <iostream>
+
 using namespace std;
 
 int x;
 
 int main()
 {
-    int y;
-    cout << x << endl;
-    cout << y << endl;
-    
-    return 0;
+	int y;
+	cout << x << endl;
+	cout << y << endl;
+	
+	return 0;
 }
+
+/***************************/
 
 #endif
