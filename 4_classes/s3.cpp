3ce27fdd85e24f41c880ddbd5b6b3a070d8ff188
AIxi0425 <aixi0425@gmail.com>
2016-07-28 20:36:59 +0800

    Special member

---
+++ 4_classes/s3.cpp
@@ -0,0 +1,34 @@
+/*
+ * Specical members
+ */
+
+#define SEG1
+
+/***************************/
+#ifdef SEG1
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Example3 {
+	string data;
+public:
+	Example3 (const string& str): data(str) {}
+	Example3 () {}
+	const string& content () const {return data;}
+};
+
+int main()
+{
+	Example3 foo;
+	Example3 bar("Example");
+
+	cout << "bar's content: " << bar.content() << endl;
+
+	return 0;
+}
+
+/***************************/
+
+#endif
