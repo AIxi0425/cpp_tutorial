79d1c384d01eff2f6366b2d1a7d42c6f4006dcd5
AIxi0425 <aixi0425@gmail.com>
2016-07-31 16:15:06 +0800

    Type conversions

---
+++ 5_other_language_features/s1.cpp
@@ -0,0 +1,40 @@
+/*
+ * Tpye conversions
+ */
+
+#define SEG1
+
+/***************************/
+#ifdef SEG1
+
+#include <iostream>
+using namespace std;
+
+class A {};
+
+class B {
+public:
+	B(const A& x) {}
+	B& operator=(const A& x)
+	{
+		return *this;
+	}
+	operator A()
+	{
+		return A();
+	}
+};
+
+int main()
+{
+	A foo;
+	B bar = foo;
+	bar = foo;
+	foo = bar;
+	
+	return 0;
+}
+
+/***************************/
+
+#endif
